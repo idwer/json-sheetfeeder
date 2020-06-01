084d28352cc6ffc9057b061b20fc4b54c188748b
Idwer Vollering <idwer@localhost>
2020-06-01 19:35:33 +0200

    add default URL handler

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+    req_data = request.get_json(force = True)
+
+    return ''
 
 if __name__ == "__main__":
     app.run(debug=True)
