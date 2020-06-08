3707eeabd232b252d9e56cea8cfe4b143fd9a583
Idwer Vollering <idwer@localhost>
2020-06-08 15:02:23 +0200

    update test_spreadsheet, untested

--- test_spreadsheet.py
+++ test_spreadsheet.py
@@ -1,6 +1,7 @@
 # source: https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 # source: https://realpython.com/python-testing/
 # source: https://rubikscode.net/2019/03/04/test-driven-development-tdd-with-python/
+import configparser
 import gspread
 import unittest
 
@@ -11,7 +12,11 @@ class TestSpreadsheet(unittest.TestCase):
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
 
-    sheet = client.open("frank_api").sheet1
+    config = configparser.ConfigParser()
+    config.read('spreadsheet.config')
+
+    sheet = client.open_by_key(config['spreadsheet']['url_key'])
+    worksheet = sheet.worksheet(config['workspace']['sheet'])
 
     def test(self):
         self.assertTrue(self.sheet, "<Worksheet 'Sheet1' id:0>")
